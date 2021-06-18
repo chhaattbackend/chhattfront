87d7553f93aa4a59a0984534684ff774d9984113
Hamza-Bhai <hm6048884hm@gmail.com>
2021-06-18 16:59:59 +0500

    pu

--- resources/views/frontend/property/search.blade.php
+++ resources/views/frontend/property/search.blade.php
@@ -2,48 +2,57 @@
 
 
 @section('style')
-    <link rel="stylesheet" type="text/css" href="./styles/home/home.css" />
     <link rel="stylesheet" type="text/css" href="./styles/property/properties.css" />
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@tarekraafat/autocomplete.js@10.1.3/dist/css/autoComplete.01.min.css">
+        
 @endsection
 
 @section('headercontent')
-    <div class="mn_div">
-          <div class="backg">
-            <div class="backg_sdiv">
-              <h1>Find the best properties from top locations</h1>
-              <div class="main_bar">
-
-              </div>
-            </div>
-          </div>
+<div class="mn_divz">
+      <div class="backgz">
+        <div class="backg_sdivz">
+          <h1>heading</h1>
         </div>
+      </div>
+    </div>
 @endsection
-
-
-
-
 @section('content')
-
     <!-- agency search start -->
     <div>
         <div class="agency-search">
             <div class="secdiv">
                 <!-- ============================  SEARCH BOX START  ================================  -->
-                {{-- <h3>Agencies Search</h3> --}}
+                <h3>Agencies Search</h3>
                 <div class="searchdiv">
                     <div class="leftSec">
-                        <span>
-                            <!-- <RiSearch2Line fontSize="26px" /> -->
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                          <form onSubmit="" class="searchdivL">
+                        <span class="searchIcon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="23" height="25" fill="#CCCCCC"
                                 class="bi bi-search" viewBox="0 0 16 16">
                                 <path
                                     d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                             </svg>
                         </span>
                         <!-- search field start  -->
-                        <div>
-                            <input type="text" placeholder="try Something 'Final'" />
+                        <!-- search start -->
+                        <div class="autoComplete_wrapper">
+                            <input id="autoComplete" autocomplete="off" type="text" tabindex="1" />
                         </div>
+                        <!-- city select start -->
+                        <select id="citiesSelect" onchange="changecity()" class="form-select"
+                            aria-label="Default select example">
+                            @foreach ($city as $item)
+                                <option value="{{ $item->name }}">{{ $item->name }} </option>
+                            @endforeach
+                        </select>
+                        <!-- city select end -->
+                        <!-- search end -->
+                        <!-- city lagana h -->
+                        <button type="submit" onclick="changeFunc(event)">
+                            Search
+                        </button>
+                    </form>
                     </div>
                 </div>
                 <div class="d-flex flex-column mt-4">
@@ -53,8 +62,6 @@
 
                             @foreach ($propertytype as $item)
                             <option   value="{{ $item->id }}">{{ $item->name }}</option>
-
-
                             @endforeach
 
                         </select>
@@ -194,5 +201,148 @@
         </div>
         <!--  PROPERTY CARD START  -->
     </div>
+
+@endsection
+@section('personalscripts')
+    <script src="https://cdn.jsdelivr.net/npm/@tarekraafat/autocomplete.js@10.1.3/dist/autoComplete.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"
+        integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous">
+    </script>
+    <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
+    <!-- EXTERNAL LINKS END -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"></script>
+    <script>
+        var city = $('#citiesSelect').children("option:selected").val();
+
+        search('karachi');
+
+        // function citiesData() {
+        // CITIES START
+        //     axios.get('https://uat.chhatt.com/api/cities').then((prev) =>
+        //         $.each(prev.data, function(key, value) {
+        //             console,log()
+        //             $("#citiesSelect").append(`
+        //         <option value="${value.id}">${value.name}</option>`);
+        //         })
+        //     )
+        // }
+        // CITIES END
+
+        // == SEARCH AREA DROPDOWN START
+        var areas;
+        var city;
+
+        function changecity() {
+            // var city = $('#citiesSelect').children("option:selected").val();
+            city = $('#citiesSelect').children("option:selected").val();
+            // console.log(city);
+            // areas = "";
+            search(city);
+        }
+
+        function search() {
+
+            const autoCompleteJS = new autoComplete({
+                data: {
+                    src: async () => {
+                        console.log(areas)
+                        try {
+                            // Loading placeholder text
+                            document
+                                .getElementById("autoComplete")
+                                .setAttribute("placeholder", "Loading...");
+                            // Fetch External Data Source
+                            const source = await fetch(
+                                `http://uat.chhatt.com/api/allareas?city=${city}`
+                            );
+                            areas = await source.json();
+                            // Post Loading placeholder text
+                            document
+                                .getElementById("autoComplete")
+                                .setAttribute("placeholder", autoCompleteJS.placeHolder);
+                            // Returns Fetched data
+                            // console.log(areas.data)
+                            return areas.data;
+                        } catch (error) {
+                            return error;
+                        }
+                    },
+                    keys: ["name"],
+                    cache: true,
+                    filter: (list) => {
+
+                        // Filter duplicates
+                        // incase of multiple data keys usage
+                        const filteredResults = Array.from(
+                            new Set(list.map((value) => value.match))
+                        ).map((food) => {
+                            return list.find((value) => value.match === food);
+                        });
+                        return filteredResults;
+                    }
+                },
+                placeHolder: "Try Something 'Final'",
+                resultsList: {
+                   
+                    // noResults: true,
+                    maxResults: 100,
+                    // tabSelect: true
+                },
+                resultItem: {
+                    element: (item, data) => {
+                        // console.log(data.match)
+                        // Modify Results Item Style
+                        item.style = "display: flex; justify-content: space-between;";
+                        // Modify Results Item Content
+                        item.innerHTML =
+                            `<span style="text-overflow: ellipsis; white-space: nowrap; overflow: hidden;">
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                        ${data.match}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                    </span>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                    <span style="margin-left:15px;display:inline-block;width:160px;text-align:right;align-items: center; font-size: 13px; font-weight: 100; text-transform: uppercase; color: rgba(0,0,0,.2); text-overflow: ellipsis; white-space: nowrap; overflow: hidden;">
+                                                                                                                                                                                                                                                                                                                                                                                                                                                    ${data.value.parent}
+                                                                                                                                                                                                                                                           </span>`;
+                    },
+                    highlight: true
+                },
+                events: {
+                    input: {
+                        selection: (e) => {
+                            const selection = e.detail.selection.value;
+                            autoCompleteJS.input.value = selection.name;
+                        },
+                        focus: () => {
+                            if (autoCompleteJS.input.value.length) autoCompleteJS.start();
+                        }
+                    }
+                }
+            });
+
+        }
+
+        // SUBMIT START
+        function changeFunc(e) {
+            e.preventDefault()
+            const inpVal = document.getElementById("autoComplete");
+            if (areas) {
+                const filteredArea = areas.data.filter((prev) => prev.name === inpVal.value)
+                if (filteredArea.length) {
+                    // console.log(filteredArea[0].key)
+                    var url = '{{ route('property.search', 'search_areas=:key') }}';
+                    url = url.replace(':key', filteredArea[0].key);
+                    document.location.href = url;
+                } else {
+                    // console.log(inpVal.value)
+                }
+            } else {
+                // console.log("error")
+            }
+        }
+        // SUBMIT END
+        // == SEARCH AREA DROPDOWN END
+
+    </script>
 
 @endsection
