3ba92ae5c0ae1a7da67261486b9995913832b80b
muddasir ali <muddasirali99@gmail.com>
2021-08-04 10:33:12 +0500

    work

--- resources/views/layouts/textcontent.blade.php
+++ resources/views/layouts/textcontent.blade.php
@@ -1,66 +1,66 @@
 <div class="textContent">
-            <div class="innerTextContent">
-                <h2 className='font-weight-bold'>
-                    Chhatt.com
-                </h2>
-                <p>
-                    If you are  looking for the best costruction material and service propvider or property
-                    and agencies then you are at the right place chhatt.com facilitates your material and property
-                    need,  just on a click at chhatt.com and switch in the world of best and discounted costruction
-                    material and professionals.
+    <div class="innerTextContent">
+        <h2 className='font-weight-bold'>
+            Chhatt.com
+        </h2>
+        <p>
+            If you are looking for the best costruction material and service propvider or property
+            and agencies then you are at the right place chhatt.com facilitates your material and property
+            need, just on a click at chhatt.com and switch in the world of best and discounted costruction
+            material and professionals.
 
-                </p>
-                <p>
-                    We assure you to provide quality material /property with best services provider/ realtors
-                    and guidance at a reasonable price, no matter if you are looking for  buying material/propperty
-                    or looking for the service propvider/agency ,  Feel free to contact us. Because we value your
-                    money and your requirement.
+        </p>
+        <p>
+            We assure you to provide quality material /property with best services provider/ realtors
+            and guidance at a reasonable price, no matter if you are looking for buying material/propperty
+            or looking for the service propvider/agency , Feel free to contact us. Because we value your
+            money and your requirement.
 
-                </p>
-                <br />
-                <div>
-                    <h3>Construction Materials</h3>
-                    <p>
-                        Chhatt.com aims to provide you the best and discounted, all types of construction material.
-                        As chhatt.com provides you the list of verified material vendors with their products which
-                        they are selling with the details and their prices, from those vendors, consumer can chosee
-                        who is selling the quality product with the best rates among those sellers.
-                    </p>
-                    <p>
-                        We worked with property real estates, buyers, sellers, investors
-                        and agents from all over Karachi so you can
-                        <a href='/property'>buy and sell property</a> easily in all
-                        areas of Karachi from chhatt.com at the best price.
-                    </p>
-                </div>
-                <br />
-                <div>
-                    <h3>professionals</h3>
-                    <p>
-                        consumer can also find proffesional  for new construcion/renovation or maintanance at chhatt.com.
-                        we aims to provide trusted proffessional who gives the quality service on a due time.
-                        consumer can find proffesionals like Architects, designers, engenieers, mason, carpainter, plumber,
-                        electrision and many more..
-                    </p>
-                </div>
-                <br />
-                <div>
-                    <h3>Property</h3>
-                    <p>
-                        chhatt.com aims to provide all type of property ranging from  residential, commercial or industrial
-                         either it is for sale, rent or booking according to their need, we connect consumer to the authorize
-                         realtor of that property.
-                    </p>
-
-                </div>
-                <div>
-                    <h3>Agencies</h3>
-                    <p>
-                        chhatt.com provides list of real estate agency by area and working area, buying/ selling/ renting
-                        property is not a easy task, you need a professional reltor who has the current knowlade of that area
-                        where you want to sell/buy the property.
-                    </p>
-
-                </div>
-            </div>
+        </p>
+        <br />
+        <div>
+            <h3>Construction Materials</h3>
+            <p>
+                Chhatt.com aims to provide you the best and discounted, all types of construction material.
+                As chhatt.com provides you the list of verified material vendors with their products which
+                they are selling with the details and their prices, from those vendors, consumer can chosee
+                who is selling the quality product with the best rates among those sellers.
+            </p>
+            <p>
+                We worked with property real estates, buyers, sellers, investors
+                and agents from all over Karachi so you can
+                <a href='/property'>buy and sell property</a> easily in all
+                areas of Karachi from chhatt.com at the best price.
+            </p>
         </div>
+        <br />
+        <div>
+            <h3>professionals</h3>
+            <p>
+                consumer can also find proffesional for new construcion/renovation or maintanance at chhatt.com.
+                we aims to provide trusted proffessional who gives the quality service on a due time.
+                consumer can find proffesionals like Architects, designers, engenieers, mason, carpainter, plumber,
+                electrision and many more..
+            </p>
+        </div>
+        <br />
+        <div>
+            <h3>Property</h3>
+            <p>
+                chhatt.com aims to provide all type of property ranging from residential, commercial or industrial
+                either it is for sale, rent or booking according to their need, we connect consumer to the authorize
+                realtor of that property.
+            </p>
+
+        </div>
+        <div>
+            <h3>Agencies</h3>
+            <p>
+                chhatt.com provides list of real estate agency by area and working area, buying/ selling/ renting
+                property is not a easy task, you need a professional reltor who has the current knowlade of that area
+                where you want to sell/buy the property.
+            </p>
+
+        </div>
+    </div>
+</div>
